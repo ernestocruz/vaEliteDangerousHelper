2b0e151f90b9581865853fa8b4294bebfbb997f0
ecruz <ernesto.cruz@live.com>
2022-04-21 15:41:24 -0400

    more proxy changes

---
+++ vaEliteDangerousHelperPlugin/vaEliteDangerousHelperPlugin/VA/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace VA.Enums
+{
+    public enum CapturedAudioTypes
+    { 
+        LastRecognizedAudio =0,
+        PreviousRecognizedAudio = 1,
+        LastUnrecognizedAudio = 2,
+        LastAudio = 3,
+        PreviousAudio = 4,
+        DictationAudio = 5
+    }
+}
--- vaEliteDangerousHelperPlugin/vaEliteDangerousHelperPlugin/VA/vaProxyAdapter.cs
+++ vaEliteDangerousHelperPlugin/vaEliteDangerousHelperPlugin/VA/vaProxyAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -99,6 +100,199 @@ namespace vaEliteDangerousHelperPlugin.VA
         {
             _vaProxy.WriteToLog(value, vaColor.GetColor(color));
         }
+        /// <summary>
+        /// Clears VA Log in main screen
+        /// </summary>
+        public void ClearLog()
+        {
+            _vaProxy.ClearLog();
+        }
+
+        /// <summary>
+        /// Sets main screen opacity level from 0 to 100
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetOpacity(int value)
+        {
+            if (value < 0 || value > 100)
+                throw new ArgumentException("Invalid opacity value. Please pass value between 0 and 100","value");
+
+            _vaProxy.SetOpacity(value);
+        }
+
+        public Version ProxyVersion
+        {
+            get {
+                return _vaProxy.ProxyVersion;
+            }
+        }
+
+        public Version VoiceAttackVersion
+        {
+            get
+            {
+                return _vaProxy.VAVersion;
+            }
+        }
+
+        public string PluginPath
+        {
+            get
+            {
+                return _vaProxy.PluginPath;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether or not the user has clicked the Stop All Commands in VA UI.
+        /// </summary>
+        /// <returns></returns>
+        public bool Stopped
+        {
+            get
+            {
+                return _vaProxy.Stopped;
+            }
+        }
+
+        /// <summary>
+        /// Returns Voice Attack Installation Directory
+        /// </summary>
+        /// <returns></returns>
+        public string InstallDir
+        {
+            get
+            {
+                return _vaProxy.InstallDir;
+            }
+        }
+
+        /// <summary>
+        /// Returns the path to the VA sounds directory.
+        /// </summary>
+        /// <returns></returns>
+        public string SoundsDirectory
+        {
+            get
+            {
+                return _vaProxy.SoundsDir;
+            }
+        }
+
+        /// <summary>
+        /// Returns path of VA Apps directory.
+        /// </summary>
+        /// <returns></returns>
+        public string AppsDirectory
+        {
+            get
+            {
+                return _vaProxy.AppsDir;
+            }
+        }
+
+        /// <summary>
+        /// Voice Attack shared assemblies directory
+        /// </summary>
+        /// <returns></returns>
+        public string AssembliesDirectory
+        {
+            get
+            {
+                return _vaProxy.AssembliesDir;
+            }
+        }
+
+        /// <summary>
+        /// Will reset the flag used by Stopped
+        /// </summary>
+        public void ResetStopFlag()
+        {
+            _vaProxy.ResetStopFalg();
+        }
+
+        /// <summary>
+        /// Returns whether the current version of VA is a released version.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsReleaseVersion
+        {
+            get
+            {
+                return _vaProxy.IsRelease;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether current version of VA is Trial Version or not
+        /// </summary>
+        /// <returns></returns>
+        public bool IsTrialVersion
+        {
+            get
+            {
+                return _vaProxy.IsTrial;
+            }
+        }
+
+        public bool PluginsEnabled
+        {
+            get
+            {
+                return _vaProxy.PluginsEnabled;
+            }
+        }
+
+        public bool NestedTokensEnabled
+        {
+            get
+            {
+                return _vaProxy.NestedTokensEnabled;
+            }
+        }
+
+        public bool AutoProfileSwitchingEnabled
+        {
+            get
+            {
+                return _vaProxy.AutoProfileSwitchingEnabled;
+            }
+        }
+
+        public IntPtr MainWindowHandle
+        {
+            get
+            {
+                return _vaProxy.MainWindowHandle;
+            }
+        }
+
+        public void Close()
+        {
+            _vaProxy.Close();
+        }
+
+        #region "Utility"
+        public string[] ExtractPhrases(string phrases, bool trimSpaces = false, bool lowercase = false)
+        {
+            return _vaProxy.Utility.ExtractPhrases(phrases, trimSpaces, lowercase);
+        }
+
+        /// <summary>
+        /// Extract value from any available token
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string ParseTokens(string value)
+        {
+            return _vaProxy.Utility.ParseTokens(value);
+        }
+
+        public MemoryStream CapturedAudio(int type)
+        {
+            return _vaProxy.Utility.CapturedAudio(type);
+        }
+        #endregion
 
     }
 }
